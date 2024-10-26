c7801dc5bb49a47c7cba1786ff3cae170d90abb8
Darius Dinu <dariusdinu19@gmail.com>
2024-10-26 13:08:41 +0300

    Created the backend project and started work on the domain entities.

---
+++ Backend/LangVet/Domain/Entities/HighlightedTerm.cs
@@ -0,0 +1,68 @@
+﻿using Domain.Common;
+
+namespace Domain.Entities
+{
+    public class HighlightedTerm
+    {
+        public HighlightedTerm(string termName, string termDefinition, string termDescription, string termLink, string termSubCluster)
+        {
+            this.termId = Guid.NewGuid();
+            this.termName = termName;
+            this.termDefinition = termDefinition;
+            this.termDescription = termDescription;
+            this.termLink = termLink;
+            this.termSubCluster = termSubCluster;
+        }
+
+        public Guid termId { get; private set; }
+        public string termName { get; private set; }
+        public string termDefinition { get; private set; }
+        public string termDescription { get; private set; }
+        public string termLink { get; private set; }
+        public string termSubCluster { get; private set; }
+
+        public static Result<HighlightedTerm> Create(string termName, string termDefinition, string termDescription, string termLink, string termSubCluster)
+        {
+            var validator = validateParameters(termName, termDefinition, termDescription, termLink, termSubCluster);
+            if (validator != null)
+            {
+                return validator;
+            }
+            return Result<HighlightedTerm>.Success(new HighlightedTerm(termName, termDefinition, termDescription, termLink, termSubCluster));
+        }
+
+        public static Result<HighlightedTerm> Update(Guid termId, string termName, string termDefinition, string termDescription, string termLink, string termSubCluster)
+        {
+            var validator = validateParameters(termName, termDefinition, termDescription, termLink, termSubCluster);
+            if (validator != null)
+            {
+                return validator;
+            }
+            var termUpdated = new HighlightedTerm(termName, termDefinition, termDescription, termLink, termSubCluster)
+            {
+                termId = termId
+            };
+            return Result<HighlightedTerm>.Success(termUpdated);
+        }
+
+        public static Result<HighlightedTerm>? validateParameters(string termName, string termDefinition, string termDescription, string termLink, string termSubCluster)
+        {
+            if (string.IsNullOrWhiteSpace(termName))
+                return Result<HighlightedTerm>.Failure("TermName should not be empty");
+
+            if (string.IsNullOrWhiteSpace(termDefinition))
+                return Result<HighlightedTerm>.Failure("TermDefinition should not be empty");
+
+            if (string.IsNullOrWhiteSpace(termDescription))
+                return Result<HighlightedTerm>.Failure("TermDescription should not be empty");
+
+            if (string.IsNullOrWhiteSpace(termLink))
+                return Result<HighlightedTerm>.Failure("TermLink should not be empty");
+
+            if (string.IsNullOrWhiteSpace(termSubCluster))
+                return Result<HighlightedTerm>.Failure("TermSubCluster should not be empty");
+
+            return null;
+        }
+    }
+}
---
+++ Backend/LangVet/Domain/Entities/Input.cs
@@ -0,0 +1,63 @@
+﻿using Domain.Common;
+
+namespace Domain.Entities
+{
+    public class Input
+    {
+        public Input(string inputText, string inputName, byte[] inputFile)
+        {
+            InputId = Guid.NewGuid();
+            InputText = inputText;
+            InputName = inputName;
+            InputFile = inputFile;
+
+        }
+
+        public Guid InputId { get; private set; }
+
+        public byte[] InputFile { get; private set; }
+
+        public string InputText { get; private set; }
+
+        public string InputName { get; private set; }
+
+        public static Result<Input> Create(string inputText, string inputName, byte[] inputFile)
+        {
+            var validator = validateParameters(inputText, inputName, inputFile);
+            if (validator != null) 
+            {
+                return validator;
+            }
+            return Result<Input>.Success(new Input(inputText, inputName, inputFile));
+        }
+
+        public static Result<Input> Update(Guid inputId, string inputText, string inputName, byte[] inputFile)
+        {
+            var validator = validateParameters(inputText, inputName, inputFile);
+            if (validator != null)
+            {
+                return validator;
+            }
+            var inputUpdated = new Input(inputText, inputName, inputFile)
+            {
+                InputId = inputId
+            };
+            return Result<Input>.Success(inputUpdated);
+        }
+
+        public static Result<Input>? validateParameters(string inputText, string inputName, byte[] inputFile)
+        {
+            if (string.IsNullOrWhiteSpace(inputText))
+                return Result<Input>.Failure("InputText should not be empty");
+
+            if (string.IsNullOrWhiteSpace(inputName))
+                return Result<Input>.Failure("InputName should not be empty");
+
+            if (inputFile.Length == 0)
+                return Result<Input>.Failure("InputFile should not be empty");
+
+            return null;
+        }
+
+    }
+}
