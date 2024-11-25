ab70f1f0e23f759282aba966c7338b05c4456650
FreaksMind <31421210+FreaksMind@users.noreply.github.com>
2024-11-25 03:01:04 +0200

    Updated MLEntityExtractor to solve import error

--- NLPService/EntityExtractors/MLEntityExtractor.py
+++ NLPService/EntityExtractors/MLEntityExtractor.py
@@ -1,14 +1,12 @@
-from EntityExtractorBase import EntityExtractorBase
-from utils import preprocessor
+from EntityExtractors.EntityExtractorBase import EntityExtractor
 
 #This class implements EntityExtractorBase class
 #The algorithm used in this class is to identify veterinary entities with the help of ML.
 #For now, we think of using a Transfomer model to identify entities. (GPT-3, BERT, etc.)
 
-class MLEntityExtractor(EntityExtractorBase):
+class MLEntityExtractor(EntityExtractor):
     def __init__(self):
         pass
 
-    @preprocessor
     def identify_entities(self):
         return "ML"
