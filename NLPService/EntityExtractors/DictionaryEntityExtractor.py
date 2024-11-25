193eb2c89df9bd567710934c5bc993c23e9fbe94
FreaksMind <31421210+FreaksMind@users.noreply.github.com>
2024-11-25 03:02:12 +0200

    Updated DictionaryEntityExtractor to solve import error & added functionality

--- NLPService/EntityExtractors/DictionaryEntityExtractor.py
+++ NLPService/EntityExtractors/DictionaryEntityExtractor.py
@@ -1,12 +1,27 @@
-from EntityExtractorBase import EntityExtractorBase
-from utils import preprocessor
+from EntityExtractors.EntityExtractorBase import EntityExtractor
+from owlready2 import *
+import utils
 #This class implements EntityExtractorBase class
-#The algorithm used in this class is to identify veterinary entities using a dictionary.
+#The algorithm used in this class is to identify veterinary entities using a corpus.
 
-class DictionaryEntityExtractor(EntityExtractorBase):
+class CorpusEntityExtractor(EntityExtractor):
+
+    ontology = None
+
     def __init__(self):
         pass
     
-    @preprocessor
-    def identify_entities(self, text):
-        return "Dictionary"
+    def load_onto(self, onto_path):
+        self.ontology = get_ontology(onto_path).load()
+
+    def identify_entities(self, text, db_instance):
+        terms = []
+        preprocessed_text = utils.preprocessor(text)
+        for cls in self.ontology.classes():
+            term = cls.label[0] if cls.label else "No label"
+            definition = cls.IAO_0000115[0] if cls.IAO_0000115 else "No definition"
+            if cls.label and term in preprocessed_text:
+                terms.append(db_instance.check_term(term, definition))
+
+        return terms
+    
