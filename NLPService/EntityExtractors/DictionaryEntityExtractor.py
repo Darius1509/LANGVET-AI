b2c6515928c3d61a1d96ae00395d0c0c7beec6a1
FreaksMind <31421210+FreaksMind@users.noreply.github.com>
2024-12-10 20:51:05 +0200

    Testing parsing at sentence level

--- NLPService/EntityExtractors/DictionaryEntityExtractor.py
+++ NLPService/EntityExtractors/DictionaryEntityExtractor.py
@@ -16,12 +16,22 @@ class CorpusEntityExtractor(EntityExtractor):
 
     def identify_entities(self, text, db_instance):
         terms = []
-        preprocessed_text = utils.preprocessor(text)
+
+        preprocessed_sentences = utils.sentences_preprocessor(text)
+
+        for sentence in preprocessed_sentences:
+            for cls in self.ontology.classes():
+                if cls.label is not None and cls.label[0] in sentence:
+                    term = cls.label[0]
+                    definition = cls.IAO_0000115[0] if cls.IAO_0000115 else "No definition"
+                    terms.append(db_instance.check_term(term, definition))
+
+        '''preprocessed_text = utils.preprocessor(text)
         for cls in self.ontology.classes():
             term = cls.label[0] if cls.label else "No label"
             definition = cls.IAO_0000115[0] if cls.IAO_0000115 else "No definition"
             if cls.label and term in preprocessed_text:
-                terms.append(db_instance.check_term(term, definition))
+                terms.append(db_instance.check_term(term, definition))'''
 
         return terms
     
