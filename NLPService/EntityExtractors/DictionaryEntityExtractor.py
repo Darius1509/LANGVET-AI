8dbfc6c45c6851424e0b6e672d680eafac1de161
ioana pește <100304166+tbfidek@users.noreply.github.com>
2024-12-16 19:56:13 +0200

    integrated the clustering part in the response

--- NLPService/EntityExtractors/DictionaryEntityExtractor.py
+++ NLPService/EntityExtractors/DictionaryEntityExtractor.py
@@ -1,5 +1,6 @@
 from EntityExtractors.EntityExtractorBase import EntityExtractor
 from owlready2 import *
+from LeidenClustering import Graph
 import utils
 #This class implements EntityExtractorBase class
 #The algorithm used in this class is to identify veterinary entities using a corpus.
@@ -15,8 +16,11 @@ class CorpusEntityExtractor(EntityExtractor):
         self.ontology = get_ontology(onto_path).load()
 
     def identify_entities(self, text, db_instance):
-        terms = []
-
+        index = 0
+        graph_nodes = {}
+        output = {}
+        output['terms'] = []
+        output['clusters'] = []
         preprocessed_sentences = utils.sentences_preprocessor(text)
 
         for sentence in preprocessed_sentences:
@@ -24,8 +28,27 @@ class CorpusEntityExtractor(EntityExtractor):
                 if cls.label is not None and cls.label[0] in sentence:
                     term = cls.label[0]
                     definition = cls.IAO_0000115[0] if cls.IAO_0000115 else "No definition"
-                    terms.append(db_instance.check_term(term, definition))
+                    term_id = db_instance.check_term(term, definition)
+                    test = {'term_id': term_id, 'context': sentence, 'term_name': term}
+                    graph_nodes[index] = test
+                    output['terms'].append(term_id)
+                    index += 1
+        
+        g = Graph(graph_nodes)
 
+        communities = g.compute_clusters(10)
+        clusters = []
+        for community in communities:
+            cluster = {}
+            cluster['nodes'] = []
+            cluster['summary'] = None
+            for node in community:
+                cluster['nodes'].append(graph_nodes[node])
+            terms_list = [entry['term_name'] for entry in graph_nodes.values()]
+            cluster['summary'] = utils.generate_cluster_description(terms_list)
+            clusters.append(cluster)
+        output['clusters'] = clusters
+        
         '''preprocessed_text = utils.preprocessor(text)
         for cls in self.ontology.classes():
             term = cls.label[0] if cls.label else "No label"
@@ -33,5 +56,5 @@ class CorpusEntityExtractor(EntityExtractor):
             if cls.label and term in preprocessed_text:
                 terms.append(db_instance.check_term(term, definition))'''
 
-        return terms
+        return output
     
