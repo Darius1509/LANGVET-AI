2a0fd9949c6cb296fea2d3857d41e6b5903a3046
ioana pește <100304166+tbfidek@users.noreply.github.com>
2025-01-14 02:55:57 +0200

    Refactored ml extractor

--- NLPService/EntityExtractors/MLEntityExtractor.py
+++ NLPService/EntityExtractors/MLEntityExtractor.py
@@ -1,21 +1,54 @@
 import json
 from utils import openai_extract_terms
 from EntityExtractors.EntityExtractorBase import EntityExtractor
-
+from LeidenClustering import Graph
+import utils
 #This class implements EntityExtractorBase class
 #The algorithm used in this class is to identify veterinary entities with the help of ML.
 #For now, we think of using a Transfomer model to identify entities. (GPT-3, BERT, etc.)
 
 class MLEntityExtractor(EntityExtractor):
+    terms = None
+
     def init(self):
         pass
 
-    def identify_entities(self, text, db_instance):
-        terms = []
+    def load_terms(self, arg):
+        self.terms = arg
 
+    def identify_entities(self, text, db_instance):
         json_terms = openai_extract_terms(text)
-        print(json_terms)
         data = json.loads(json_terms)
+        index = 0
+        graph_nodes = {}
+        output = {}
+        output['terms'] = []
+        output['clusters'] = []
         for term in data:
-            terms.append(db_instance.check_term(term['term'], term['description']))
-        return terms
+            term_id = self.terms.get(term['term'])[0]
+            if term_id is None:
+                term_id = db_instance.insert_term(term, term['description'])
+                self.terms[term] = [term_id, term['description']]
+            node = {'term_id': term_id, 'context': term['sentence'], 'term_name': term['term']}
+            graph_nodes[index] = node
+            output['terms'].append(term_id)
+            index += 1
+
+        g = Graph(graph_nodes)
+
+        communities = g.compute_clusters()
+        clusters = []
+        for community in communities:
+            cluster = {}
+            cluster['nodes'] = []
+            cluster['summary'] = None
+            terms_list = []
+            for node in community:
+                cluster['nodes'].append(graph_nodes[node])
+                terms_list.append(graph_nodes[node]['term_name'])
+            print(terms_list)
+            cluster['summary'] = utils.generate_cluster_description(terms_list)
+            clusters.append(cluster)
+        output['clusters'] = clusters
+
+        return output
